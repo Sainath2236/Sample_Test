f87bb4e584f5c02b64e1b88b12d5ca624b9c01d3
Sainath2236 <machunursainathgoud@gmail.com>
2017-12-09 15:02:33 +0530

    first project structure

---
+++ Test/src/com/test/sample1/pojo/Login.java
@@ -0,0 +1,5 @@
+package com.test.sample1.pojo;
+
+public class Login {
+
+}
---
+++ Test/src/sample/test/sample2/pojo/Login.java
@@ -0,0 +1,9 @@
+package sample.test.sample2.pojo;
+
+public class Login {
+	public static void main(String args[]){
+		System.out.println("sample test");
+	}
+	
+
+}
